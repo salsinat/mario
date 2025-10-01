a3ed5b02ebdeeab904b40a7061ff61f0098afde6
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-10-01 11:20:49 +0200

    Ouverture d'une fenêtre

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
+
+#-------------------------------------- IMPORTATION --------------------------------------#
 from sys import exit # déclaration d'importation du module sys
 import pygame # déclaration d'importation du module pygame
 
 pygame.init() # initialisation du module pygame. Il est obligatoire d'appeler cette fonction une seule fois après l'avoir importé
+
+#-------------------------------------- INITIALISATION FENÊTRE DE JEU --------------------------------------#
+
+screen = pygame.display.set_mode((400,300))
+pygame.display.set_caption("MARIO BROS")
