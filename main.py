d413d659b38c884582aacc9918917975c97eb601
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-10-01 11:13:45 +0200

    First commit: pygame importation

---
+++ main.py
@@ -0,0 +1,4 @@
+from sys import exit # déclaration d'importation du module sys
+import pygame # déclaration d'importation du module pygame
+
+pygame.init() # initialisation du module pygame. Il est obligatoire d'appeler cette fonction une seule fois après l'avoir importé
