7b4408ec973c6ac1d42f269a127e49552292e108
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-10-01 11:23:01 +0200

    Fenêtre qui se ferme que quand on quitte

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ pygame.init() # initialisation du module pygame. Il est obligatoire d'appeler ce
 
 screen = pygame.display.set_mode((400,300))
 pygame.display.set_caption("MARIO BROS")
+
+#-------------------------------------- MAJ DE L'ETAT DE JEU --------------------------------------#
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
