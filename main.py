a2857a295218564a22c5d6e8a792badbd924de63
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-10-01 11:36:34 +0200

    Carré qui bouge

--- main.py
+++ main.py
@@ -3,19 +3,49 @@
 #-------------------------------------- IMPORTATION --------------------------------------#
 from sys import exit # déclaration d'importation du module sys
 import pygame # déclaration d'importation du module pygame
+from pygame.locals import *
 
 pygame.init() # initialisation du module pygame. Il est obligatoire d'appeler cette fonction une seule fois après l'avoir importé
 
-#-------------------------------------- INITIALISATION FENÊTRE DE JEU --------------------------------------#
+#-------------------------------------- INITIALISATION FENÊTRE DE JEU ET DES VARIABLES --------------------------------------#
 
 screen = pygame.display.set_mode((400,300))
 pygame.display.set_caption("MARIO BROS")
+running = True;
+COLOR_BLACK = Color(0, 0, 0)
+COLOR_RED = Color(255, 0, 0)
+
+#-------------------------------------- CREATION DU PERSONNAGE --------------------------------------#
+square = Rect(5, 5, 50, 50)
+mov_speed = 5
 
 #-------------------------------------- MAJ DE L'ETAT DE JEU --------------------------------------#
 
-while True:
+while running:
+    # On "repeint" l'écran en noir afin de ne pas laisser l'ancien carré
+    screen.fill(COLOR_BLACK)
+
+    """ Gestion des évènements """
+    key_pressed = pygame.key.get_pressed()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    pygame.display.update()
+        if event.type == pygame.QUIT or key_pressed[K_ESCAPE]:
+            running = False
+        
+        # Gestion de l'appluie des flêches directionnelles du clavier pour bouger notre carré
+        if key_pressed[K_UP]:
+            square.y -= mov_speed
+        elif key_pressed[K_DOWN]:
+            square.y += mov_speed
+        if key_pressed[K_LEFT]:
+            square.x -= mov_speed
+        elif key_pressed[K_RIGHT]:
+            square.x += mov_speed
+    
+    """ Logique du jeu """
+    pygame.draw.rect(screen, COLOR_RED, square)
+
+    """ Rafraichissement """ 
+    pygame.display.flip()
+
+pygame.quit()
+exit()
